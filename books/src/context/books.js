b5279a5ad7706601d700132b114cd2dfe2506b62
Matheus Barbosa <mattbarb93@gmail.com>
2024-10-05 16:57:39 -0400

    useCallback function

--- books/src/context/books.js
+++ books/src/context/books.js
@@ -1,15 +1,16 @@
 import { createContext, useState } from "react";
 import axios from "axios";
+import { useCallback } from "react";
 
 const BooksContext = createContext();
 
 function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
-  };
+  }, []);
 
   const createBook = async (title) => {
     const response = await axios.post("http://localhost:3001/books", {
